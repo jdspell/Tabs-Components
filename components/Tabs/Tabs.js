b48a522b11075c4668a81c362fa26f0595572ed8
jdspell <spelljd15@gmail.com>
2019-01-31 20:02:00 -0500

    trying to get deselect methods to work for tabs class

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -32,7 +32,10 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener("click", () => this.select());
+    this.element.addEventListener("click", () => {
+      this.select();
+      this.deselect();
+    })
   };
 
   select() {
@@ -40,7 +43,7 @@ class TabLink {
     const links = document.querySelectorAll(".tabs-link");
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach(link => link.classList.remove("tabs-link-selected"));
+    // Array.from(links).forEach(link => link.classList.remove("tabs-link-selected"));
 
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add("tabs-link-selected");
@@ -49,8 +52,8 @@ class TabLink {
     this.tabItem.select();
   }
 
-  deselect(){
-
+  deselect(selectedTab){
+    tab.classList.remove("tabs-link-selected");
   }
 }
 
@@ -65,16 +68,16 @@ class TabItem {
     const items = document.querySelectorAll(".tabs-item");
 
     // Remove the class "tabs-item-selected" from each element
-    items.forEach(item => {
-      item.classList.remove("tabs-item-selected");
-    })
+    // items.forEach(item => {
+    //   item.classList.remove("tabs-item-selected");
+    // })
     
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add("tabs-item-selected");
   }
 
-  deselect(){
-
+  deselect(selectedTab){
+    selectedTab.classList.remove("tabs-item-selected");
   }
 }
 
